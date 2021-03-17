5dc3b8aaebf47090f69d5a117a3e84409ddbe376
GRANIER ANTOINE <agrani03@localhost.localdomain>
2021-03-17 17:35:20 +0100

    mise à jour classes

--- src/fr/dut/info/Card.java
+++ src/fr/dut/info/Card.java
@@ -1,5 +1,15 @@
 package fr.dut.info;
 
 public class Card {
-
+	private final String name;
+	private final String rarity;
+	private final int energyCost;
+	private final String type;
+	public Card(String name, String rarity, int energyCost, String type) {
+		this.name = name;
+		this.rarity = rarity;
+		this.energyCost = energyCost;
+		this.type = type;
+	}
+	
 }
--- src/fr/dut/info/Room.java
+++ src/fr/dut/info/Room.java
@@ -3,4 +3,17 @@ package fr.dut.info;
 public class Room {
 	private final Player player;
 	private final UI ui;
+	public Room(Player player, UI ui) {
+		this.player = player;
+		this.ui = ui;
+	}
+	public void enterRoom(Player player) {
+		
+	}
+	public String getRoomType() {
+		
+	}
+	public UI getUi() {
+		
+	}
 }
